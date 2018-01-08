872c58ef0bcba05ff77e88fe811d8a0c2c65e152
Clare <chblackburne@gmail.com>
2018-01-08 17:57:56 +0000

    Adds list css

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -5,7 +5,10 @@ class MoviesList extends React.Component{
   render(){
     const movieNodes = this.props.data.map(movie => {
       return(
-        <Movie showTime={movie.showTime} key={movie.id}>{movie.title}</Movie>
+        <section class="movieItem">
+          <img src="plusSign.jpg"/><div class="movieDetails"><Movie showTime={movie.showTime} key={movie.id}><p class="title">{movie.title}</p><a class="times" href="#">Showtimes</a></Movie>
+        </div>
+      </section>
       );
     });
     return(
--- src/components/OpeningsBox.js
+++ src/components/OpeningsBox.js
@@ -6,7 +6,9 @@ class OpeningsBox extends React.Component {
     super(props);
     this.state = {
       data: [
-        {id: 1, title: "Casablanca", showTime: "Monday 1:00pm"}
+        {id: 1, title: "Casablanca", showTime: "Monday 1:00pm"},
+        {id: 2, title: "Reservoir Dogs", showTime: "Tuesday 8:00pm"},
+        {id: 3, title: "Apocalypse Now", showTime: "Wednesday 10:00pm"},
       ]
     }
   }
@@ -17,6 +19,10 @@ class OpeningsBox extends React.Component {
         <h1>UK Openings This Week</h1>
       </header>
       <MoviesList data={this.state.data}/>
+      <div id="linkText">
+        <a href="#">See more opening this week<span id="linkArrows">>></span></a>
+      </div>
+      <hr />
       <footer>
         <button id="showtimesBtn">Get Showtimes >></button>
       </footer>
